7e7a54319321b511b14e423d2b180a55b7327c39
John Mercier <moaxcp@gmail.com>
2020-05-09 21:12:46 -0400

    Adding DisplayName parsing with tests

--- src/main/java/com/github/moaxcp/x11client/DisplayName.java
+++ src/main/java/com/github/moaxcp/x11client/DisplayName.java
@@ -1,9 +1,39 @@
 package com.github.moaxcp.x11client;
 
 import lombok.NonNull;
+import lombok.Value;
 
+import static com.github.moaxcp.x11client.Strings.requireNonBlank;
+
+@Value
 public class DisplayName {
-  public DisplayName(@NonNull String displayName) {
+  String hostName;
+  int displayNumber;
+  int screenNumber;
 
+  /**
+   * Creates a new display name.
+   * @param displayName
+   * @throws NullPointerException
+   * @throws NumberFormatException if displayNumber or screenNumber is not a number
+   */
+  public DisplayName(@NonNull String displayName) {
+    requireNonBlank("displayName", displayName);
+    int colonIndex = displayName.indexOf(':');
+    if(colonIndex == -1) {
+      hostName = displayName;
+      displayNumber = 0;
+      screenNumber = 0;
+      return;
+    }
+    hostName = colonIndex == 0 ? null : displayName.substring(0, colonIndex);
+    int dotIndex = displayName.indexOf('.');
+    if(dotIndex == -1) {
+      displayNumber = Integer.parseInt(displayName.substring(colonIndex + 1));
+      screenNumber = 0;
+    } else {
+      displayNumber = Integer.parseInt(displayName.substring(colonIndex + 1, dotIndex));
+      screenNumber = Integer.parseInt(displayName.substring(dotIndex + 1));
+    }
   }
 }
--- src/test/java/com/github/moaxcp/x11client/DisplayNameTest.java
+++ src/test/java/com/github/moaxcp/x11client/DisplayNameTest.java
@@ -7,8 +7,50 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DisplayNameTest {
   @Test
-  void constructor() {
+  void constructor_fails_on_null_displayName() {
     NullPointerException exception = assertThrows(NullPointerException.class, () -> new DisplayName(null));
     assertThat(exception).hasMessage("displayName is marked non-null but is null");
+  }
+
+  @Test
+  void constructor_fails_on_blank_displayName() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new DisplayName(" "));
+    assertThat(exception).hasMessage("displayName must not be blank");
+  }
+
+  @Test
+  void constructor_hostName_only() {
+    DisplayName name = new DisplayName("hostName");
+    assertThat(name.getHostName()).isEqualTo("hostName");
+  }
+
+  @Test
+  void constructor_displayNumber_not_a_number() {
+    NumberFormatException exception = assertThrows(NumberFormatException.class, () -> new DisplayName(":asdb"));
+    assertThat(exception).hasMessage("For input string: \"asdb\"");
+  }
+
+  @Test
+  void constructor_displayNumber_only() {
+    DisplayName name = new DisplayName(":12");
+    assertThat(name.getHostName()).isNull();
+    assertThat(name.getDisplayNumber()).isEqualTo(12);
+    assertThat(name.getScreenNumber()).isEqualTo(0);
+  }
+
+  @Test
+  void constructor_displayNumber_withScreenNumber() {
+    DisplayName name = new DisplayName(":12.2");
+    assertThat(name.getHostName()).isNull();
+    assertThat(name.getDisplayNumber()).isEqualTo(12);
+    assertThat(name.getScreenNumber()).isEqualTo(2);
+  }
+
+  @Test
+  void constructor_all() {
+    DisplayName name = new DisplayName("hostName:12.2");
+    assertThat(name.getHostName()).isEqualTo("hostName");
+    assertThat(name.getDisplayNumber()).isEqualTo(12);
+    assertThat(name.getScreenNumber()).isEqualTo(2);
   }
 }
