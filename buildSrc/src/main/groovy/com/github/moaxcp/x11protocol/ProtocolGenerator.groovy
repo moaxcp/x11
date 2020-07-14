2441cdbb5c8e0c76f22ed6c0e16030098ace43e5
John Mercier <moaxcp@gmail.com>
2020-07-13 22:20:09 -0400

    Adding class

--- buildSrc/src/main/groovy/com/github/moaxcp/x11protocol/ProtocolGenerator.groovy
+++ buildSrc/src/main/groovy/com/github/moaxcp/x11protocol/ProtocolGenerator.groovy
@@ -1,5 +1,9 @@
 package com.github.moaxcp.x11protocol
 
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.TypeSpec
+import groovy.util.slurpersupport.GPathResult
+
 class ProtocolGenerator {
     InputStream inputStream
     File outputSrc
@@ -11,5 +15,28 @@ class ProtocolGenerator {
     void generate() {
         def root = new XmlSlurper().parse(inputStream)
         fullPackage = basePackage + ".${root.@header}"
+        def format = root.breadthFirst().find { it.name() == 'struct' && it.@name == 'FORMAT' }
+        if(format) {
+            createStruct(format)
+        }
+    }
+
+    void createStruct(GPathResult struct) {
+        String name = struct.@name
+        StringBuilder className = new StringBuilder()
+        if(name.capitalize() == name) {
+            className.append(name.charAt(0))
+            className.append(name.substring(1).toLowerCase())
+        } else {
+            className.append(name)
+        }
+        def type = TypeSpec.classBuilder(className.toString())
+
+        struct.childNodes()
+
+        def javaFile = JavaFile.builder(fullPackage, type.build())
+                .build()
+
+        javaFile.writeTo(outputSrc)
     }
 }
