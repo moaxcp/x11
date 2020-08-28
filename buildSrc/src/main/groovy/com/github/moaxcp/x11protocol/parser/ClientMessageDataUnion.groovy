00dffac66991e873b7cf6cbb2365c814aed11baa
John Mercier <moaxcp@gmail.com>
2020-08-27 21:45:30 -0400

    fixing ClientMessageDataUnion

--- buildSrc/src/main/groovy/com/github/moaxcp/x11protocol/parser/ClientMessageDataUnion.groovy
+++ buildSrc/src/main/groovy/com/github/moaxcp/x11protocol/parser/ClientMessageDataUnion.groovy
@@ -1,10 +1,7 @@
 package com.github.moaxcp.x11protocol.parser
 
-import com.squareup.javapoet.ClassName
-import com.squareup.javapoet.MethodSpec
-import com.squareup.javapoet.TypeName
-import com.squareup.javapoet.TypeSpec
-import javax.lang.model.element.Modifier 
+import com.squareup.javapoet.*
+import javax.lang.model.element.Modifier
 
 class ClientMessageDataUnion extends JavaUnion {
     @Override
@@ -24,6 +21,27 @@ class ClientMessageDataUnion extends JavaUnion {
             .addParameter(ClassName.get(basePackage, 'X11Input'), 'in')
             .addParameter(TypeName.BYTE, 'format')
             .addException(IOException)
+            .addCode(CodeBlock.builder()
+                .beginControlFlow('switch(format)')
+                .add('case 8:\n')
+                .indent()
+                .addStatement('return new $T(in.readCard8(20))', ClassName.get(javaPackage, 'ClientMessageData8'))
+                .unindent()
+                .add('case 16:\n')
+                .indent()
+                .addStatement('return new $T(in.readCard16(10))', ClassName.get(javaPackage, 'ClientMessageData16'))
+                .unindent()
+                .add('case 32:\n')
+                .indent()
+                .addStatement('return new $T(in.readCard32(5))', ClassName.get(javaPackage, 'ClientMessageData32'))
+                .unindent()
+                .add('default:\n')
+                .indent()
+                .addStatement('throw new IllegalArgumentException("format must be 8, 16, or 32. Got: " + format + ".")')
+                .unindent()
+                .endControlFlow()
+                .build()
+            )
             .build()
     }
 
